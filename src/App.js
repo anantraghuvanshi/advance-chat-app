a34d091aa2818310fe33799bf3f78aa4684b4d38
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-30 23:55:59 +0530

    components add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine } from 'react-chat-engine';
 
-import './App.css';
+import ChatFeed from './components/ChatFeed';
+import './App.css';;
+
+const App = () =>{
+    return (
+        <ChatEngine 
+            height = '100vh'
+            projectID='5895d14d-bb72-4769-b676-165ce7ba3010'
+            userName= 'Anant'
+            userSecret='123123'
+            renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps} />}
+        />
+
+    );
+}
+
+export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MessageForm from './MessageForm';
+import MyMessage from '/MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+
+    
+  return (
+    <div>ChatFeed</div>
+  )
+}
+
+export default ChatFeed;
